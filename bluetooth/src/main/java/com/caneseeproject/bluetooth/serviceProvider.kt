f00d142e7f0a4073e20747b4aeceb29a217d3864
toqa <toqa7elshahat@gmail.com>
2020-01-26 21:42:31 +0200

    connected with HC05

--- bluetooth/src/main/java/com/caneseeproject/bluetooth/serviceProvider.kt
+++ bluetooth/src/main/java/com/caneseeproject/bluetooth/serviceProvider.kt
@@ -1,90 +1,56 @@
 package com.caneseeproject.bluetooth
-
 import android.bluetooth.BluetoothAdapter
 import android.bluetooth.BluetoothDevice
-import android.bluetooth.BluetoothServerSocket
 import android.bluetooth.BluetoothSocket
-import android.content.BroadcastReceiver
-import android.content.ContentValues.TAG
-import android.content.Context
-import android.content.Intent
-import android.content.IntentFilter
+import android.hardware.SensorDirectChannel
 import android.util.Log
-import android.widget.Toast
-
-
 import com.caneseeproject.sensorPortals.Sensor
 import com.caneseeproject.sensorPortals.SensorPortal
 import java.io.IOException
 import java.util.*
 
 
- class serviceProvider ()  {
+ class ServiceProvider(): SensorPortal  {
+     val bluetoothAdapter: BluetoothAdapter? = BluetoothAdapter.getDefaultAdapter()
+     val mydevice : String = "98:D3:61:FD:66:FB"
+     var device = bluetoothAdapter!!.getRemoteDevice(mydevice)
 
-    /* val filter = IntentFilter(BluetoothDevice.ACTION_ACL_CONNECTED)
-
-     val receiver = object : BroadcastReceiver() {
-         override fun onReceive(context: Context, intent: Intent) {
-             val action: String = intent.action
-             when(action) {
-                 BluetoothDevice.ACTION_ACL_CONNECTED -> {
-                     Toast.makeText(context, "CCCOONNNEECCTTEEEDD", Toast.LENGTH_LONG)
-                 }
-
-                 }
-
-         }
-
-     }
-     init {
-         context.registerReceiver(receiver, filter)
-     }*/
-      class ConnectionThreed(device: BluetoothDevice) : Thread(){
-
-
-        val MY_UUID = UUID.fromString("e7f78a05-42f8-4f5d-8886-d57efb14dcef")
-        val bluetoothAdapter: BluetoothAdapter? = BluetoothAdapter.getDefaultAdapter()
-        private val mmSocket: BluetoothSocket? by lazy(LazyThreadSafetyMode.NONE) {
-            device.createRfcommSocketToServiceRecord(MY_UUID)
-
-        }
-
-        public override fun run() {
-            // Cancel discovery because it otherwise slows down the connection.
-            bluetoothAdapter?.cancelDiscovery()
-
-            mmSocket?.use { socket ->
-                // Connect to the remote device through the socket. This call blocks
-                // until it succeeds or throws an exception.
-                socket.connect()
-                Log.i("MYACTIVITY", "WEEE AARREEE CCOONNEECCTTEEDD")
-
-                // The connection attempt succeeded. Perform work associated with
-                // the connection in a separate thread.
-                //manageMyConnectedSocket(socket)
+     override fun connect(bluetoothDevice: BluetoothDevice, flag : Boolean) :Sensor {
+         val sensordata = sensorData()
+         val MY_UUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB")
+         val bluetoothAdapter: BluetoothAdapter? = BluetoothAdapter.getDefaultAdapter()
+         val socket: BluetoothSocket = device.createRfcommSocketToServiceRecord(MY_UUID)
+         bluetoothAdapter!!.cancelDiscovery()
+         if(flag==true){
+             socket.connect()
+             Log.i("MYACTIVITY", "we are connected")
             }
 
-        }
-
-        // Closes the client socket and causes the thread to finish.
-        fun cancel() {
-            try {
-                mmSocket?.close()
-            } catch (e: IOException) {
-                Log.e(TAG, "Could not close the client socket", e)
-
+         if(flag == false){
+             socket.close()
+             Log.i("MYACTIVITY", " socket is closed")
             }
+
+         return sensordata
+
+
+
         }
 
 
 
+    fun startBluetooth(){
+        if (bluetoothAdapter?.isEnabled == false) {
+            bluetoothAdapter.enable()
+            Log.i("MYACTIVITY", "on")
+
+        }
     }
+    fun endBluetooth(){
+        if (bluetoothAdapter?.isEnabled == true) {
+            bluetoothAdapter.disable()
+            Log.i("MYACTIVITY", "of")
 
-
-
-
-
-
-
-
+        }
+    }
  }
