eb2af1b21a3ff7665628a9b2657ab624c7bdffe0
lolla <aliaezz42@gmail.com>
2020-01-27 02:56:03 +0200

    first edition

--- computervision/src/main/java/InterCV.kt
+++ computervision/src/main/java/InterCV.kt
@@ -1,14 +1,23 @@
 package com.caneseeaproject.computervision
 
+import com.caneseeproject.sensorPortals.SensorInput
 import com.caneseeproject.sensorPortals.SensorReading
 import kotlinx.coroutines.flow.Flow
 import org.json.JSONObject
 
 
 interface InterCV {
-    //fun sendParsedData(data: Flow<JSONObject>)
-    suspend fun sendDataUp(rawData: SensorReading) //output data(in string) to the above layer
-    suspend fun getDataBelow(rawData: SensorReading):Flow<String> //get data(in string) from the below layer
-    //fun getParsedData():Flow<JSONObject>
+
+    fun sendData():Flow<cvReading.getMode>//output data(in string) to the above layer
+    suspend fun setMode(mode: cvInput) //app detect mode of glasses
+
+}
+
+open class cvReading: SensorReading{
+    class getMode(val mode: Int) :cvReading()
+}
+
+open class cvInput: SensorInput{
+    class getData(val rawData:String ): cvInput()
 
 }
