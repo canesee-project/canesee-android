f5e0577fd9ff80c81269cb68159fe35d7d25755d
toqa <toqa7elshahat@gmail.com>
2020-01-29 03:06:04 +0200

    readings -> error in return type

--- bluetooth/src/main/java/com/caneseeproject/bluetooth/sensorData.kt
+++ bluetooth/src/main/java/com/caneseeproject/bluetooth/sensorData.kt
@@ -1,14 +1,46 @@
 package com.caneseeproject.bluetooth
 
+import android.bluetooth.BluetoothServerSocket
+import android.bluetooth.BluetoothSocket
+import android.text.util.Rfc822Tokenizer
+import android.text.util.Rfc822Tokenizer.tokenize
 import com.caneseeproject.sensorPortals.*
+import kotlinx.coroutines.channels.*
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.consumeAsFlow
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.runBlocking
+import java.io.InputStream
+import java.io.OutputStream
 
-class sensorData : Sensor{
-    override fun readings(tokenize: ReadingTokenizer): Flow<SensorReading> {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+class sensorData(private  val socket: BluetoothSocket) : Sensor{
+    val mmInStream: InputStream = socket.inputStream
+    val mmBuffer: ByteArray = ByteArray(1024)
+
+    override fun <T : SensorReading> readings(tokenize: ReadingTokenizer<T>): Flow<T> {
+        var sringed_flow :ReceiveChannel<Char> = Channel()
+        var reciever : Channel<String> = Channel()
+        fun producer() = runBlocking {
+            sringed_flow = produce {
+                mmInStream.read(mmBuffer).toString().forEach {
+                    send(it)
+                    reciever.consumeEach { it }
+                }
+            }
+
+        }
+        val result: Flow<SensorReading> = reciever.consumeAsFlow().map { tokenize(it) }
+        return result
+
+
     }
 
-    override suspend fun send(encode: InputEncoder, vararg messages: SensorInput) {
+
+
+
+
+
+    override suspend fun <T : SensorInput> send(encode: InputEncoder<T>, vararg messages: T) {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
@@ -19,7 +51,5 @@ class sensorData : Sensor{
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
-
-
-
 }
+
--- bluetooth/src/main/java/com/caneseeproject/bluetooth/serviceProvider.kt
+++ bluetooth/src/main/java/com/caneseeproject/bluetooth/serviceProvider.kt
@@ -10,43 +10,18 @@ import java.io.IOException
 import java.util.*
 
 
- class ServiceProvider(): SensorPortal  {
+ class BluetoothSensorPortal(private val MAC : String): SensorPortal  {
      val bluetoothAdapter: BluetoothAdapter? = BluetoothAdapter.getDefaultAdapter()
-     val mydevice : String = "98:D3:61:FD:66:FB"
-     var device = bluetoothAdapter!!.getRemoteDevice(mydevice)
-
-     override fun connect(bluetoothDevice: BluetoothDevice, flag : Boolean) :Sensor {
-         val sensordata = sensorData()
-
+    // val HC05 : String = "98:D3:61:FD:66:FB"
+     var device = bluetoothAdapter!!.getRemoteDevice(MAC)
+     override fun connect() :Sensor {
          val MY_UUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB")
          val bluetoothAdapter: BluetoothAdapter? = BluetoothAdapter.getDefaultAdapter()
          val socket: BluetoothSocket = device.createRfcommSocketToServiceRecord(MY_UUID)
+         val sensordata = sensorData(socket)
          bluetoothAdapter!!.cancelDiscovery()
-         if(flag==true){
-             socket.connect()
-             Log.i("MYACTIVITY", "we are connected")
-            }
-
-         if(flag == false){
-             socket.close()
-             Log.i("MYACTIVITY", " socket is closed")
-            }
-
+         socket.connect()
+         Log.i("MYACTIVITY", "we are connected")
          return sensordata
-
         }
-    fun startBluetooth(){
-        if (bluetoothAdapter?.isEnabled == false) {
-            bluetoothAdapter.enable()
-            Log.i("MYACTIVITY", "on")
-
-        }
-    }
-    fun endBluetooth(){
-        if (bluetoothAdapter?.isEnabled == true) {
-            bluetoothAdapter.disable()
-            Log.i("MYACTIVITY", "of")
-
-        }
-    }
  }
