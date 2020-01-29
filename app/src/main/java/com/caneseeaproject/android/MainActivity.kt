d2fa84cda9180863a95b98c5d5f9081a0d1b6f7b
mhashim6 <m6hashim@gmail.com>
2020-01-29 18:18:44 +0200

    added CaneSeeService.

--- app/src/main/java/com/caneseeaproject/android/MainActivity.kt
+++ app/src/main/java/com/caneseeaproject/android/MainActivity.kt
@@ -1,12 +1,39 @@
 package com.caneseeaproject.android
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.ComponentName
+import android.content.Intent
+import android.content.ServiceConnection
 import android.os.Bundle
+import android.os.IBinder
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var caneSee: CaneSeeService
+    private val serviceConnection = object : ServiceConnection {
+        override fun onServiceDisconnected(name: ComponentName?) {
+        }
+
+        override fun onServiceConnected(name: ComponentName?, service: IBinder) {
+            caneSee = (service as CaneSeeService.CaneSeeServiceBinder).caneSee
+        }
+
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        connectCaneSeeService()
+    }
+
+    private fun connectCaneSeeService() {
+        Intent(this, CaneSeeService::class.java).also {
+            bindService(it, serviceConnection, 0)
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        unbindService(serviceConnection)
     }
 }
