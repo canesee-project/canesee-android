5f3ce14f9b44ad896a9f1703349c357eb5d3549b
ayafikry33 <ayafikry33@gmail.com>
2020-01-31 15:00:29 +0200

    tts

--- tts/src/main/java/com/caneseeproject/tts/tts.kt
+++ tts/src/main/java/com/caneseeproject/tts/tts.kt
@@ -1,6 +1,7 @@
 package com.caneseeproject.tts
 
 import android.speech.tts.TextToSpeech
+import android.util.Log
 import java.util.*
 
 fun main(args: Array<String>){
@@ -9,19 +10,34 @@ fun main(args: Array<String>){
 }
 
 interface texttospeach {
-    var tts: TextToSpeech? = null
-    fun speakOut(text: Int )
+    fun onInit(status: Int)
+    fun speakOut(text: Int)
    // fun onDestroy()
+
 }
 
 class texttospeachInstance : texttospeach {
+    var tts: TextToSpeech? = null
 
+    override fun onInit(status: Int) {
+
+        if (status == TextToSpeech.SUCCESS) {
+            // set US English as language for tts
+            val result = tts!!.setLanguage(Locale.US)
+
+            if (result == TextToSpeech.LANG_MISSING_DATA || result == TextToSpeech.LANG_NOT_SUPPORTED) {
+                Log.e("TTS","The Language specified is not supported!")
+            }
+        } else {
+            Log.e("TTS", "Initilization Failed!")
+        }
+    }
     override fun speakOut(text: Int) {
         //tts function implementation
         val textString = text.toString()
         tts!!.speak(textString, TextToSpeech.QUEUE_FLUSH, null);
     }
-    /*override fun onDestroy() {
+    /* override fun onDestroy() {
         if (tts != null) {
             tts!!.stop()
             tts!!.shutdown()
