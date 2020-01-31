2a48a5a2eb497810e46a0e56c854a111dcb08045
ayafikry33 <ayafikry33@gmail.com>
2020-01-31 14:32:34 +0200

    tts

--- tts/src/main/java/com/caneseeproject/tts/tts.kt
+++ tts/src/main/java/com/caneseeproject/tts/tts.kt
@@ -1,17 +1,30 @@
 package com.caneseeproject.tts
 
+import android.speech.tts.TextToSpeech
+import java.util.*
+
 fun main(args: Array<String>){
-    var myVariable = texttospeach()
-    myVariable.tts()
+    var myVariable = texttospeachInstance()
+    myVariable.speakOut(25469)
 }
 
-interface tts{
-
-    fun tts()
+interface texttospeach {
+    var tts: TextToSpeech? = null
+    fun speakOut(text: Int )
+   // fun onDestroy()
 }
 
-class texttospeach : tts{
-    override fun tts() {
+class texttospeachInstance : texttospeach {
 
+    override fun speakOut(text: Int) {
+        //tts function implementation
+        val textString = text.toString()
+        tts!!.speak(textString, TextToSpeech.QUEUE_FLUSH, null);
     }
+    /*override fun onDestroy() {
+        if (tts != null) {
+            tts!!.stop()
+            tts!!.shutdown()
+        }
+    }*/
 }
