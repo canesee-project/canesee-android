9279c657ba2e18e3152c85d48bc00be929f9fc2f
ayafikry33 <ayafikry33@gmail.com>
2020-02-05 12:09:27 +0200

    tts

--- app/src/main/java/com/caneseeaproject/android/MainActivity.kt
+++ app/src/main/java/com/caneseeaproject/android/MainActivity.kt
@@ -2,12 +2,9 @@ package com.caneseeaproject.android
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.caneseeaproject.tts.*
-import com.caneseeproject.tts.TextToSpeachClass
 
 
 class MainActivity : AppCompatActivity() {
-    var x = TextToSpeachClass()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
