775da6de4c5fd17abf6a3ba52ab61c2a7ae1507d
mhashim6 <m6hashim@gmail.com>
2020-02-13 12:02:24 +0200

    rpi0 MAC

--- app/src/main/java/com/caneseeaproject/android/Utils.kt
+++ app/src/main/java/com/caneseeaproject/android/Utils.kt
@@ -6,7 +6,7 @@ import android.util.Log
  *@author mhashim6 on 2020-01-29
  */
 
-const val CANE_MAC = "98:D3:61:FD:66:FB"
+const val CANE_MAC = "98:D3:61:FD:66:FB" //pi0: B8:27:EB:15:7C:75
 const val GLASSES_MAC = "B8:27:EB:B4:73:E9"
 
 
