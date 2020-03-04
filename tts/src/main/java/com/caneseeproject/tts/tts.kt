e441699250afffdf253b30e3efae9a0fee88eef3
ayafikry33 <ayafikry33@gmail.com>
2020-03-04 19:51:32 +0200

    الاخير ان شاء الله

--- tts/src/main/java/com/caneseeproject/tts/tts.kt
+++ tts/src/main/java/com/caneseeproject/tts/tts.kt
@@ -17,33 +17,34 @@ class TextToSpeachClass : TextToSpeechInterface {
         tts!!.speak(text, TextToSpeech.QUEUE_FLUSH, null)
     }
 
-    override fun notify(recent: NotificationType) {
+    override fun notify ( recent : NotificationType ) {
         when (recent) {
-            is NotificationType.SensorNotification -> when (recent.sensorType) {
-                is Vision.OCR -> speakOut("OCR is Activated now")
-                is Vision.Facial -> speakOut("face recognition is turned on")
-                is Vision.Emotion -> speakOut("emotion recognition is turned on")
-                is Vision.ObjectDetection -> speakOut("object detection is turned on")
-                is Vision.Scenery -> speakOut("scene Description")
-                is ODReading.ObstacleDistance -> speakOut("Distance Detection is turned on")
+            is NotificationType.SensorNotification -> when (recent.state) {
+                is CVInput.ModeChange -> when (recent.state.mode){
+                    is 1 -> speakOut("OCR is Activated now")
+                    is 2 -> speakOut("face recognition is turned on")
+                    is 3 -> speakOut("emotion recognition is turned on")
+                    is 4 -> speakOut("object detection is turned on")
+                    is 5 -> speakOut("scene Description")
+                }
+                is ODInput.RangeControl -> speakOut("Distance Detection is turned on")
+            }
 
+            is NotificationType.SensorContent -> when (recent.context) {
+                is Vision.OCR -> speakOut(recent.context.transcript)
+                is Vision.Facial -> speakOut(recent.context.prettyFace)
+                is Vision.Emotion -> speakOut(recent.context.emotion)
+                is Vision.ObjectDetection -> speakOut(recent.context.objects.toString())
+                is Vision.Scenery -> speakOut(recent.context.scene)
+                is ODReading.ObstacleDistance -> speakOut(recent.context.distance.toString())
             }
-            is NotificationType.SensorContent -> when (recent.textReading) {
-                is Vision.OCR -> speakOut(recent.textReading.transcript)
-                is Vision.Facial -> speakOut(recent.textReading.prettyFace)
-                is Vision.Emotion -> speakOut(recent.textReading.emotion)
-                is Vision.ObjectDetection -> speakOut(recent.textReading.objects.toString())
-                is Vision.Scenery -> speakOut(recent.textReading.scene)
-                is ODReading.ObstacleDistance -> speakOut(recent.textReading.distance.toString())
-            }
-            is NotificationType.AppMessage -> when{
 
-            }
+            is NotificationType.AppMessage -> speakOut(recent.appTutorial)
         }
     }
 }
 sealed class NotificationType{
-    class AppMessage :NotificationType()
-    class SensorNotification(val sensorType :SensorReading) :NotificationType()
-    class SensorContent(val textReading :SensorReading) :NotificationType()
+    class AppMessage(val appTutorial:String) :NotificationType()
+    class SensorNotification(val state :SensorInput) :NotificationType()
+    class SensorContent(val context :SensorReading) :NotificationType()
 }
