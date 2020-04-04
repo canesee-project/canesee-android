f4c12df21e44957336c3f19e3a09d387a326f7b4
mhashim6 <m6hashim@gmail.com>
2020-04-04 13:01:36 +0200

    adapt latest renaming in app module.

--- app/src/main/java/com/caneseeaproject/android/CaneSeeService.kt
+++ app/src/main/java/com/caneseeaproject/android/CaneSeeService.kt
@@ -3,11 +3,12 @@ package com.caneseeaproject.android
 import android.app.Service
 import android.content.Intent
 import android.os.Binder
-import com.caneseeaproject.computervision.CVInput
+import com.caneseeaproject.computervision.CVControl
 import com.caneseeaproject.computervision.ComputerVision
 import com.caneseeaproject.computervision.Vision
 import com.caneseeproject.bluetooth.BluetoothPortal
-import com.caneseeproject.obstacledetection.ODInput
+import com.caneseeproject.obstacledetection.ODControl
+import com.caneseeproject.obstacledetection.ODReading
 import com.caneseeproject.obstacledetection.ObstacleDetector
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
@@ -43,7 +44,7 @@ class CaneSeeService : Service(), CoroutineScope {
         }
     }
 
-    fun controlGlasses(action: CVInput) = launch {
+    fun controlGlasses(action: CVControl) = launch {
         glasses.setMode(action)
     }
 
@@ -53,12 +54,19 @@ class CaneSeeService : Service(), CoroutineScope {
         useCane()
     }
 
-    fun controlCane(action: ODInput) = launch {
+    fun controlCane(action: ODControl) = launch {
         cane.control(action)
     }
 
     private fun useCane() = launch {
-        cane.detectObstacles().collect { obstacle -> outLoud(obstacle.distance.toString()) }
+        cane.readings().collect { reading ->
+            outLoud(
+                when (reading) {
+                    is ODReading.ObstacleDistance -> reading.distance.toString()
+                    is ODReading.GlassesMode -> "OD glasses mode: $reading"
+                }
+            )
+        }
     }
 
     private fun outLoud(what: String) = main.launch {
